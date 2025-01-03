27dc4dd37b8da07228e3daa32c002bd21fc3db1b
Ed_Jinton <121737321+BlackSarkk@users.noreply.github.com>
2025-01-03 08:32:08 +0000

    1

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -4,8 +4,36 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+const { Query } = require("mongoose");
 
+
+// sort string 1,
+// sort string 2,
+// if str1 = str2 => true
+// else => false
+
+function isAnagram(str1, str2) {
+  
+  const lowerCaseString1 = str1.toLowerCase();
+  const arr1 = lowerCaseString1.split("")     //har => ["h", "a", "r"]
+  arr1.sort();                                //["a", "h", "r"]
+  const sortedString1 = arr1.join("");        //"ahr"
+
+
+  const lowerCaseString2 = str2.toLowerCase();
+  const arr2 = lowerCaseString2.split("");     
+  arr2.sort();                    
+  const sortedString2 = arr2.join("");  
+
+
+
+  if (sortedString1 == sortedString2){
+    return true
+  } else{
+    return false
+  }
 }
+
+
 
 module.exports = isAnagram;
